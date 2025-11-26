1a14c630eb7c488574458579071f9d834b877b7d
raymondmar61 <raym61@hotmail.com>
2025-11-25 22:43:19 -0800

    Convert integer to string and back to integer

---
+++ codewars7kyusquareeverydigit.py
@@ -0,0 +1,35 @@
+#https://www.codewars.com/kata/546e2562b03326a88e000020/train/python
+'''
+Square Every Digit 7kyu
+
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 9^2 is 81 and 1^2 is 1. (81-1-1-81)
+
+Example #2: An input of 765 will/should return 493625 because 7^2 is 49, 6^2 is 36, and 5^2 is 25. (49-36-25)
+
+Note: The function accepts an integer and returns an integer.
+
+Happy Coding!
+'''
+def square_digits(num):
+    numberstring = str(num)
+    output = ""
+    for eachnumberstring in numberstring:
+        output += str(int(eachnumberstring) * int(eachnumberstring))
+    return int(output)
+
+
+print(square_digits(9119)) #print 811181
+print(square_digits(765)) #print 493625
+print(square_digits(0)) #print 0
+
+
+numberinteger = 765
+numberstring = str(numberinteger)
+print(numberstring) #print 765
+output = ""
+for eachnumberstring in numberstring:
+    output += str(int(eachnumberstring) * int(eachnumberstring))
+print(output) #print 493625
+print(int(output)) #print 493625
