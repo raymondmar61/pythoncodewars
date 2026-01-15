39d8309e69a82fbad3a69ebdccb7a5328a7b5e28
raymondmar61 <raym61@hotmail.com>
2026-01-14 23:03:14 -0800

    Double the integer value

---
+++ codewars8kyuyoucantcodeunderpressure01.py
@@ -0,0 +1,15 @@
+#https://www.codewars.com/kata/53ee5429ba190077850011d4/train/python
+'''
+You Can't Code Under Pressure #1 8kyu
+
+Code as fast as you can! You need to double the integer and return it.
+'''
+def double_integer(i):
+    return i * 2
+
+
+print(double_integer(2)) #print 4
+print(double_integer(4)) #print 8
+print(double_integer(-10)) #print -20
+print(double_integer(0)) #print 0
+print(double_integer(100)) #print 200
