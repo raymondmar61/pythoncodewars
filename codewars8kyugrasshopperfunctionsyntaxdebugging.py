8a37ae632e1fa71f225bc596dcc2a5c9fbe8dcce
raymondmar61 <raym61@hotmail.com>
2026-02-17 20:36:43 -0800

    Functions

---
+++ codewars8kyugrasshopperfunctionsyntaxdebugging.py
@@ -0,0 +1,15 @@
+#https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/train/python
+'''
+Grasshopper - Function syntax debugging 8kyu
+
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+def main [verb, noun]
+return verb + noun
+'''
+def main(verb, noun):
+    return verb + noun
+
+
+print(main("take ", "item")) #print take item
+print(main("use ", "sword")) #print use sword
