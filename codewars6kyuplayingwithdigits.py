5d4092b48bb21a3ac6ca9cca6f0f0f0360105370
raymondmar61 <raym61@hotmail.com>
2026-04-22 14:44:03 -0700

    Added a user submission

--- codewars6kyuplayingwithdigits.py
+++ codewars6kyuplayingwithdigits.py
@@ -75,3 +75,10 @@ while True:
         print(-1)
         break
     findk += 1
+
+#User submission
+def dig_pow(n, p):
+    s = 0
+    for i, c in enumerate(str(n)):
+        s += pow(int(c), p + i)
+        return s / n if s % n == 0 else -1
